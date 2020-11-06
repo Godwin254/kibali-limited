20905864ec0a9062fd29afe1c020f83bc7b791d7
Godwin254 <godwinbee15@gmail.com>
2020-11-06 18:24:32 +0300

    Add new stylesheet file Added styles to navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../stylesheet/navbarstyle.css'
 
 function NavBar() {
      return (
-          <div>
+          <div className="navbar">
+
+               <div className="navlink">
+
+                         <div className="search-icon list">
+                              <p>Search icon</p>
+                         </div>
+
+                         <div className="cart list">
+                              <p>Cart</p>
+                         </div>
+
+                         <div className="alert list">
+                              <p>Alert</p>
+                         </div>
+
+                         <div className="sort list">
+                              <p>Sort</p>
+                         </div>
+
+               </div>
                
           </div>
      )
