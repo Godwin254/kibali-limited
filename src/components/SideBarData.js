657cf02130fb070d8280af48d145b3f9e5d3621f
Godwin254 <godwinbee15@gmail.com>
2020-11-10 11:40:17 +0300

    Refactor side bar data

---
+++ src/components/SideBarData.js
@@ -0,0 +1,36 @@
+
+
+export const SideBarData = [
+     {
+          title: "Home",
+          path: '/',
+          icon: "icon"
+     },
+     {
+          title: "Comodities",
+          path: '/comodities',
+          icon: "icon"
+     },
+     {
+          title: "Sales track",
+          path: '/sales-track',
+          icon: "icon"
+     },
+     {
+          title: "Suppliers",
+          path: '/suppliers',
+          icon: "icon"
+     },
+     {
+          title: "Settings",
+          path: '/settings',
+          icon: "icon"
+     },
+     {
+          title: "About app",
+          path: '/about',
+          icon: "icon"
+     }
+
+     
+];
